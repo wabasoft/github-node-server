876b38d3d3b475f8b453c7a846b9a305e663f585
Xavier Baez <xavierbaez@gmail.com>
2022-11-13 02:16:44 -0800

    npm start script

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const server = http.createServer ( function(request,response){
+
+    response.writeHead(200,{"Content-Type":"text\plain"});
+    if(request.method == "GET")
+    {
+        response.end("received GET request.")
+    }
+    else if(request.method == "POST")
+    {
+        response.end("received POST request.");
+    }
+    else
+    {
+        response.end("Undefined request .");
+    }
+});
+
+server.listen(4567);
+console.log("Server running on port 4567");
